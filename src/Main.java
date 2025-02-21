13736adbf35acd9ac76d7acca779295bed7ab075
Basal2006 <15895227103@163.com>
2025-02-21 11:25:10 +0800

    my first commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        
+
+        // 创建第一个 MusicBox 对象
         MusicBox mbox = new MusicBox("S34TG65", 'Y', "Raindrops", "Misty", 
-        "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+            "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
 
         System.out.println("Now playing the song");
         mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
-        
-        // --- TODO
 
-        /* Repeat the above code to create another object  
-         * Pass different parameters to the MusicBox constructor, but initialise premiumSong to N and when calling the
-         * playSong method, pass ads parameter as just 1
-         * 
-         * 
-        */
         
+        MusicBox mbox2 = new MusicBox("T78HJ91", 'N', "Sunshine", "Aurora", 
+            "The bright side", "Pop", "Star Records", "Melody Lane");
 
+        System.out.println("Now playing the second song");
+        mbox2.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 1); // ads 参数设置为 1
     }
 }
-
 
 
 
